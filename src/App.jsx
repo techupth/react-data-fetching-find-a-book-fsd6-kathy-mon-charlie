72f79394fcee34c9fd8f4b465f4e8162fa7fed3c
Mon <tracer.coolkid@gmail.com>
2024-06-07 14:34:24 +0700

    feat:add find a book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    searchText && searchFromServer(searchText);
+  }, [searchText]);
+  
+
+  const searchFromServer = async (text) => {
+    try {
+      const response =
+        await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}
+`);
+      setSearchResult(response.data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a book</h1>
+      <input type="text" onChange={(i) => setSearchText(i.target.value)} />
+      {searchResult.length > 0 && (
+        <>
+          {searchResult.map((result, index) => (
+            <div key={index} className="searchItem">
+              <li className="result">{result.volumeInfo.title}</li>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
