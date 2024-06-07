299969e28573749f96e4d9b962586105b32f08b4
CharlieHii <charlie_chaper@hotmail.com>
2024-06-07 15:55:13 +0700

    feat: add search engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [word, setWord] = useState("");
+  const [data, setData] = useState([]);
+  const getData = async (word) => {
+    try {
+      const result = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${word}`
+      );
+      setData(result?.data.items ?? []);
+    } catch (error) {
+      setData([]);
+    }
+  };
+  useEffect(() => {
+    word ? getData(word) : setData([]);
+  }, [word]);
+  return (
+    <div className="App">
+      {/* start coding here */}
+      <h1>Find a Book</h1>
+      <input
+        placeholder="search me"
+        value={word}
+        onChange={(event) => {
+          setWord(event.target.value);
+        }}
+      />
+      <ul>
+        {data
+          .filter((item) =>
+            item.volumeInfo?.title.toLowerCase().includes(word.toLowerCase())
+          )
+          .map((item, index) => (
+            <li key={index}>{item.volumeInfo?.title}</li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
