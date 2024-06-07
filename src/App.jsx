03f4b556bda392da139c95baa0048325c5b7fdba
Mon <tracer.coolkid@gmail.com>
2024-06-07 15:36:49 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,22 @@ function App() {
   useEffect(() => {
     searchText && searchFromServer(searchText);
   }, [searchText]);
-  
 
   const searchFromServer = async (text) => {
-    try {
-      const response =
-        await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}
+    const response =
+      await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}
 `);
-      setSearchResult(response.data.items);
-    } catch (error) {
-      console.log(error);
-    }
+    setSearchResult(response.data.items);
   };
 
   return (
     <div className="App">
       <h1>Find a book</h1>
-      <input type="text" onChange={(i) => setSearchText(i.target.value)} />
+      <input
+        type="text"
+        placeholder="Search your book"
+        onChange={(i) => setSearchText(i.target.value)}
+      />
       {searchResult.length > 0 && (
         <>
           {searchResult.map((result, index) => (
